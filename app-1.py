b960b03c699b79cb3b952da069da5ffa5e8ae97f
Achmad Dino Saputra <67891619+dinosptr@users.noreply.github.com>
2022-11-22 09:49:40 +0700

    Add files via upload

---
+++ app-1.py
@@ -0,0 +1,78 @@
+# https://youtu.be/pI0wQbJwIIs
+"""
+
+Werkzeug provides a bunch of utilities for developing WSGI-compliant applications. 
+These utilities do things like parsing headers, sending and receiving cookies, 
+providing access to form data, generating redirects, generating error pages when 
+there's an exception, even providing an interactive debugger that runs in the browser. 
+Flask then builds upon this foundation to provide a complete web framework.
+"""
+
+from flask import Flask, render_template, request, redirect, flash
+from PIL import Image
+# import os
+import tensorflow as tf
+import io
+import json
+import keras
+import numpy as np
+from datetime import datetime
+# import pytz
+import torch
+# from matplotlib import pyplot as plt
+import numpy as np
+# import cv2
+# import itertools
+import os 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "asw"
+
+#Add Post method to the decorator to allow for form submission. 
+# @app.route('/', methods=['GET','POST'])
+model = keras.models.load_model("model/HAM10000_100epochs.h5")
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/best.pt', force_reload=True)
+classes = ['Actinic keratoses', 'Basal cell carcinoma', 
+               'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
+               'Melanocytic nevi', 'Vascular lesions']
+@app.route("/predict", methods=["POST"])
+def predict_api():
+    if 'file' not in request.files:
+        value = {
+            "msg": 'Tidak ada file',
+        }
+        return json.dumps(value)
+    file = request.files['file']
+    if file.filename == '':
+        value = {
+            "msg": 'Tidak ada file dipilih untuk diupload'
+        }
+        return json.dumps(value)
+    
+    # with open(file,'rb') as f:
+    image_bytes = file.read()
+    img = Image.open(io.BytesIO(image_bytes))
+    img = np.asarray(img)
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    img = tf.image.resize(img, [32, 32])
+
+    predictions = model(img)
+    predictions = tf.nn.softmax(predictions)
+    pred0 = predictions[0]
+    label0 = np.argmax(pred0)
+
+    # timeasia = pytz.timezone('Asia/Jakarta') 
+    # now = datetime.now(timeasia)
+
+    return {
+        "name": classes[label0],
+        # "date": now.strftime("%d/%m/%Y %H:%M:%S")
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
